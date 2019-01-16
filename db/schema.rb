e30ba20100d993f2b8790b0701a0573b4d77d0da
Nathan Bolser <nathan.bolser@travelershaven.com>
2019-01-16 16:07:28 -0700

    Add Shouts#new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_174334) do
+ActiveRecord::Schema.define(version: 2019_01_16_230043) do
+
+  create_table "shouts", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shouts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
