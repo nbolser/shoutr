1da20bc6eaf0b14688199d5250a63e72e7e1c1fb
Nathan Bolser <nbolser@gmail.com>
2019-01-24 12:10:03 -0700

    Add folowers relation and index

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,20 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :shouts, dependent: :destroy
+
   has_many :likes
   has_many :liked_shouts, through: :likes, source: :shout
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
+
+  has_many :followed_user_relationships,
+    foreign_key: :follower_id,
+    dependent: :destroy
+  has_many :followed_users, through: :followed_user_relationships
+
+  has_many :follower_relationships,
+    foreign_key: :followed_user_id,
+    class_name: 'FollowingRelationship',
+    dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   validates :username, presence: true, uniqueness: true
 
