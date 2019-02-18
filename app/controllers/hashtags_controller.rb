5959149adbc44133026c888131d766b9d9c63941
Nathan Bolser <nbolser@gmail.com>
2019-02-18 12:51:39 -0700

    Add hashtag search functionality

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,8 +1,5 @@
 class HashtagsController < ApplicationController
   def show
-    @hashtag = params[:id]
-    @results = Shout.
-      joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").
-      where('text_shouts.body LIKE ?', "#%#{@hashtag}%")
+    @search = Search.new(term: params[:id])
   end
 end
